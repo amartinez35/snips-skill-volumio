5bf59541643ff76531218269eeeee47eae6f2da1
Aurélien Martinez <aurelien.mrt@gmail.com>
2019-09-14 22:25:11 +0200

    test

--- action-volumio.py
+++ action-volumio.py
@@ -4,8 +4,6 @@
 from hermes_python.hermes import Hermes
 from Volumio import Volumio
 import requests
-import json
-
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -15,7 +13,7 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 def intent_received(hermes, intent_message):
 
   if intent_message.intent.intent_name == 'amartinez35:music_action' or intent_message.intent.intent_name == 'amartinez35:not_music_action':
-    available_slots = json.loads(intent_message.custom_data)
+    available_slots = intent_message.slots
 
     artist = intent_message.slots.Artist.first().value or available_slots['Artist']
     song = intent_message.slots.Song.first().value or available_slots['Song']
