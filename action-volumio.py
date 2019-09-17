ce63ea168290b79e8a6e4e14dd337c9ac98d59b3
Aurélien Martinez <aurelien.mrt@gmail.com>
2019-09-17 21:46:41 +0200

    test

--- action-volumio.py
+++ action-volumio.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
+import json
+import configparser
+import io
 from hermes_python.hermes import Hermes
 from Volumio import Volumio
 import requests
@@ -20,26 +22,60 @@ SLOTS_LIST = [
 INTENTS_LIST = ['amartinez35:music_action', 'amartinez35:not_music_action']
 
 
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {
+            section: {
+                option_name: option
+                for option_name, option in self.items(section)
+            }
+            for section in self.sections()
+        }
+
+
+def read_configuration_file():
+    try:
+        with io.open(
+            "config.ini",
+            encoding="utf-8"
+        ) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error):
+        return dict()
+
+def get_room(room):
+  config = read_configuration_file()
+  if len(room) == 0:
+    return config['secret']['salon']
+  return room
+
+def read_slots(intent_message):
+  available_slots = intent_message.slots
+  slots_values = {}
+
+  for slot in SLOTS_LIST:
+    if len(available_slots[slot]) > 0:
+      slots_values[slot] = available_slots[slot].first().value
+    else:
+      slots_values[slot] = '' 
+  
+  return slots_values
+
+
+
 def intent_received(hermes, intent_message):
 
   if intent_message.intent.intent_name in INTENTS_LIST:
-    available_slots = intent_message.slots
-    slots_values = {}
-    message = ''
+    slots_values = read_slots(intent_message)
 
-    for slot in SLOTS_LIST:
-      if len(available_slots[slot]) > 0:
-        slots_values[slot] = available_slots[slot].first().value
-      else:
-        slots_values[slot] = ''
-    
-    if len(slots_values['Piece']) == 0:
-      slots_values['Piece'] = '192.168.0.25'
+    room = get_room(slots_values['Piece'])
 
     if len(slots_values['VolumioAction']) == 0:
       message = 'Je n\'ai pas compris la demande'
     else:
-      mpd = Volumio(slots_values['Piece'])
+      mpd = Volumio(room)
       action = slots_values['VolumioAction']
       print(action)
       if action == 'demarre':
